8e80efe165ee3cc4871cd094da019ef406c575f1
Maxime <maxime.b2494@gmail.com>
2024-06-11 14:41:58 +0200

    feature: UI de home

--- lib/controller/home.dart
+++ lib/controller/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:learn_music/model/bar_choice.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -8,10 +9,54 @@ class Home extends StatefulWidget {
 
 class HomeState extends State<Home> {
 
+  int currentIndex = 0;
+  List<BarChoice> items = [
+    BarChoice(label: "Musique", iconData: Icons.music_note, page: Container()),
+    BarChoice(label: "Favoris", iconData: Icons.whatshot, page: Container()),
+    BarChoice(label: "Recherche", iconData: Icons.search, page: Container()),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(100),
+        child: SafeArea(
+          child: Container(
+            padding: const EdgeInsets.all(10),
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    Spacer(),
+                    Icon(Icons.radio),
+                    Icon(Icons.doorbell),
+                    Icon(Icons.settings)
+                  ],
+                ),
+                Text("MaxTube")
+              ],
+            ),
+          ),
+        ),
+      ),
+      body: Center(
+        child: Text("MaxTube"),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIndex,
+        items: items.map((i) => i.item).toList(),
+        onTap: barTapped,
+      ),
     );
+  }
+
+  barTapped(int index) {
+    setState(() {
+      currentIndex = index;
+    });
   }
 }
